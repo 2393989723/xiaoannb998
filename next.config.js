91c30ca2e358cacc09fa1d32664a82bdb9f1a493
2393989723 <1619746209@qq.com>
2025-09-24 11:17:39 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  cssLoaderOptions: {
-    url: false, // 禁用CSS中url加载器，避免本地资源引入报错
-  },
 };
-
 module.exports = nextConfig;
+
