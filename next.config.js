bcc9cc3330aa802378eaffa52802f378b385a368
2393989723 <1619746209@qq.com>
2025-09-24 11:31:50 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    swcFileReading: true,
+    swcWasm: true, // 强制使用WASM版SWC
+  },
 };
 module.exports = nextConfig;
+
 
