3238648574a00ba08bd607c02f6b77d66356c737
2393989723 <1619746209@qq.com>
2025-09-24 13:29:54 +0800

    Create admin.js

---
+++ pages/admin.js
@@ -0,0 +1,16 @@
+// pages/admin.js
+import Head from 'next/head';
+
+export default function Admin() {
+  return (
+    <>
+      <Head>
+        <title>管理员页面</title>
+      </Head>
+      <div>
+        <h1>管理员控制台</h1>
+        {/* 可添加管理员功能逻辑 */}
+      </div>
+    </>
+  );
+}
